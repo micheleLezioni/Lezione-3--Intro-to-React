de8c61f32c911eaeec9e4c74e068d281efeabe71
micheleLezioni <55858102+micheleLezioni@users.noreply.github.com>
2019-10-17 09:42:50 +0200

    Add files via upload

---
+++ LessonRecap.js
@@ -0,0 +1,73 @@
+/*3) Crea una funzione che presa come input una stringa ritorna la stringa inversa
+SUGGERIMENTO: Cerca su MDN la funzione reverse()
+*/
+
+/* 
+//
+//React.createElement("div", { className: "red" }, "Children Text");
+//<div className: "red"> Children Text </div>
+//FUNCTIONAL COMPONENT NON HANNO LO STATE, NON HANNO IL RENDER METHOD MA POSSONO ACCEDERE ALLE PROPS
+//I COMPONENT DI REACT SONO SCRITTI IN JSX
+//OGNI FUNCTIONAL COMPONENT PUO' ESSERE TRASFORMATO IN UN CLASS COMPONENT
+//FRAGMENT <> </> E' UN CONTENITORE CHE SERVE A CONTENERE GLI ELEMENTI JSX
+
+/* FUNCTIONAL COMPONENTE
+function App(props){
+return (
+      <>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Ciao !!!!</p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React, OK!
+            </a>
+          </header>
+        </div>
+        <div>ciao</div>
+      </> 
+}
+*/
+
+/*
+
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {};
+    }
+  
+    render() {
+      return (
+        <>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>Ciao !!!!</p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React, OK!
+              </a>
+            </header>
+          </div>
+          <div>ciao</div>
+        </>
+      );
+    }
+  }
+  
+  export default App;
+  
+
+*/
---
+++ introToReact.js
@@ -0,0 +1,39 @@
+//Prerequisiti React
+{
+  /* Cosa serve sapere?
+class,destructuring, high order method (map,forech,filter),
+arrow function, fetch api e promises */
+}
+//destructuring
+//const { nome, cognome} = persona //oggetto persona con altri campi oltre a nome e cognome
+
+//TIPI DI COMPONENT IN REACT:
+//1) SONO ATOMICI, SONO RIUTILIZZABILI, REAGISCONO A DUE
+//PROPRIETA' FONDAMENTALI DI REACT CHE SONO LE PROPS, E LO STATE
+//SONO DI 2 TIPI:
+//1) CLASS COMPONENT
+//2) FUNCTIONAL COMPONENT
+// REACT REAGISCE AL CAMBIAMENTO DELLO STATE, E DELLE PROPS
+
+//React
+{
+  /*Component GATTO
+  
+class Gatto extends Animal {
+constructor(props) {
+  super(props)
+
+  this.state = { //QUESTO STATE è LOCALE ED è ACCESSIBILE SOLO ALL'INTERNO DEL COMPONENT GATTO
+     verso : "miaoh"
+  }
+}
+
+    render (){
+      return ( this.state.verso )
+    => react element nel virtual dom che cambia al cambiare dello state 
+    => react quindi controlla cosa è cambiato ( comparandolo con il vero DOM) e farà l'update SOLO delle parti che sono cambiate
+
+    }
+}
+*/
+}
